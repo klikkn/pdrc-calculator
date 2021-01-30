e64802f98732b9834f50eb0e534870ddc626e2ed
Sergey <klikkn@yandex.ru>
2021-01-30 14:05:29 +0100

    swagger: fields update

--- apps/api/src/app/modules/orders/orders.dto.ts
+++ apps/api/src/app/modules/orders/orders.dto.ts
@@ -6,6 +6,7 @@ import {
 } from 'class-validator';
 import { IOrder, IOrderItem } from '@pdrc/api-interfaces';
 import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class OrderCreateRequestDto implements IOrder {
   constructor(partial: Partial<OrderCreateRequestDto>) {
@@ -15,43 +16,56 @@ export class OrderCreateRequestDto implements IOrder {
   @IsEmpty()
   _id?: string;
 
+  @ApiProperty({ required: false })
   carModel: string;
 
+  @ApiProperty({ required: false })
   carProducer: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   category: string;
 
+  @ApiProperty({ required: false })
   clientName: string;
 
+  @ApiProperty({ required: false })
   clientPhone: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   date: Date;
 
+  @ApiProperty({ type: () => OrderItemDto })
   @ArrayNotEmpty()
   @ValidateNested({ each: true })
   @Type(() => OrderItemDto)
   items: OrderItemDto[];
 
+  @ApiProperty()
   @IsNotEmpty()
   ownerId: string;
 }
 
 class OrderItemDto implements IOrderItem {
+  @ApiProperty()
   @IsNotEmpty()
   column: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   count: number;
 
+  @ApiProperty()
   @IsNotEmpty()
   part: string;
   row: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   table: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   value: number;
 }
